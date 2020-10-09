eb322fab26a6de832405b99a47806b199c48462c
Brian Hayes <bjoeyhayes@gmail.com>
2020-10-09 15:58:40 -0400

    update testing

---
+++ src/simplyAnimate.test.ts
@@ -0,0 +1,10 @@
+import animationSeries from './simply-animate';
+
+describe('animationSeries', () => {
+  it('should import', () => {
+    expect(animationSeries).toBeTruthy();
+  });
+
+  it('Should run when element is passed');
+  it('Should properly execute hooks');
+});
