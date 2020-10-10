4c385f92628dd768bc7a7c3f82622c92ac0e5fb1
Brian Hayes <bjoeyhayes@gmail.com>
2020-10-10 02:38:39 -0400

    Update simplyAnimate.test.ts

--- src/simplyAnimate.test.ts
+++ src/simplyAnimate.test.ts
@@ -4,7 +4,16 @@ describe('animationSeries', () => {
   it('should import', () => {
     expect(animationSeries).toBeTruthy();
   });
-
+  it('should run in most basic form', () => {
+    animationSeries({
+      seriesClassName: 'hello',
+      steps:           [
+        {
+          duration: 500
+        }
+      ]
+    });
+  });
   it('Should run when element is passed');
   it('Should properly execute hooks');
 });
