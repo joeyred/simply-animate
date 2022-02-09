92f63728eab1ebe32feb17a53496df01b25e6290
Brian Hayes <bjoeyhayes@gmail.com>
2022-02-09 04:25:32 -0500

    Update Demo - Back to Webpack

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,15 +1,41 @@
 import {animationSeries} from '../src/simply-animate';
+// import {animationSeries} from './simply-animate';
 
 animationSeries({
-  seriesClassName: 'demo',
+  seriesClassName: 'hello',
   hooks:           {
-    before: () => console.log('before!'),
-    after:  () => console.log('after!')
+    before:          () => console.log('series:before'),
+    beforeEachStep:  () => console.log('series:beforeEachStep'),
+    duringEachStep:  () => console.log('series:duringEachStep'),
+    beforeEachFrame: () => console.log('series:beforeEachFrame'),
+    afterEachFrame:  () => console.log('series:afterEachFrame'),
+    afterEachStep:   () => console.log('series:afterEachStep'),
+    after:           () => console.log('series:after')
   },
   steps: [
     {
-      name:     'hello',
-      duration: 300
+      duration: 1,
+      hooks:    {
+        before:          () => console.log('step:before'),
+        during:          () => console.log('step:during'),
+        beforeEachFrame: () => console.log('step:beforeEachFrame'),
+        afterEachFrame:  () => console.log('step:afterEachFrame'),
+        after:           () => console.log('step:after')
+      }
     }
   ]
 });
+
+// animationSeries({
+//   seriesClassName: 'demo',
+//   hooks:           {
+//     before: () => console.log('before!'),
+//     after:  () => console.log('after!')
+//   },
+//   steps: [
+//     {
+//       name:     'hello',
+//       duration: 300
+//     }
+//   ]
+// });
--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -3,7 +3,8 @@ const config = {
   entry: `${__dirname}/demo.ts`,
   output: {
     path: __dirname,
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/'
   },
   resolve: {
     // Add `.ts` and `.tsx` as a resolvable extension.
@@ -25,22 +26,27 @@ const config = {
           'css-loader',
           {
             loader:  'sass-loader',
-            options: {
-              additionalData: '$env: ' + process.env.NODE_ENV + ';',
-            }
+            // options: {
+            //   additionalData: '$env: ' + process.env.NODE_ENV + ';',
+            // }
           }
         ]
       }
     ]
   },
   devServer: {
-    contentBase:      __dirname,
-    publicPath:       '/dist/',
-    watchContentBase: true,
+    static: [
+      './dist',
+      './demo'
+    ],
+    // contentBase:      __dirname,
+    // publicPath:       '/dist/',
+    // watchFiles: true,
+    // watchContentBase: true,
     port:             8000
   }
 };
 
-console.log(config);
+console.dir(JSON.stringify(config, null, 2));
 
 module.exports = config;
