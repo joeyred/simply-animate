797d1031d48ddb78653088867705feb6d4c69c33
Brian Hayes <bjoeyhayes@gmail.com>
2022-02-26 22:28:07 -0500

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,14 @@
-const TEST_ENV = process.env.TEST_ENV;
-
-function getModule(env) {
-  if (env === 'ci') {
-    return `<rootdir>/dist/simply-animate.js`;
-  }
-  return `<rootdir>/src/simply-animate.ts`;
-}
-
-function getRoot(env) {
-  console.log(env);
-  if (env === 'ci') {
-    return '<rootDir>/build-tests';
-  }
-  return '<rootDir>/src';
-}
-
 module.exports = {
-  roots: [
-    getRoot(TEST_ENV)
-  ],
-  moduleNameMapper: {
-    '^simply-animate$': getModule(TEST_ENV)
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    pretendToBeVisual: true
   },
+  roots: [
+    '<rootDir>/src'
+  ],
+  // moduleNameMapper: {
+  //   '^simply-animate$': getModule(TEST_ENV)
+  // },
   testMatch: [
     "**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)"
@@ -29,4 +16,6 @@ module.exports = {
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest"
   },
+  collectCoverage: true,
+  coverageDirectory: './.coverage'
 }
